36451c6ec451b964fc733e4cfe525cbaad579194
David Tresnak <122625462+dvdtrsnk@users.noreply.github.com>
2023-11-20 12:05:36 +0100

    Initial Commit

---
+++ WebViewLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewLearning
+//
+//  Created by David Třešňák on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewLearning/WebViewLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewLearningApp.swift
+//  WebViewLearning
+//
+//  Created by David Třešňák on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WebViewLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
